388c826920eb6c592489b3d316b631c098baaa1a
MMirelli <massimilianomirelli.mm@gmail.com>
2019-02-25 10:14:03 +0300

    Modifying startup file

--- src/MATLAB/startup.m
+++ src/MATLAB/startup.m
@@ -3,11 +3,15 @@
 %   >> path_to_local_folder = 'your_path_to_SCA'
 %   >> save .path_to_SCA.mat
 
-file_mat = strcat(pwd, filesep, '.path_to_SCA.mat');
-load(file_mat)
-my_dir = strcat(path_to_local_folder,'src/MATLAB/');
-cd(my_dir)
-
+p=pwd;
+% Checks if the MATLAB environment has been set correctly; if not it sets
+% the path
+if ~strcmp(p(end-9:end), 'src\MATLAB')
+    file_mat = strcat(pwd, filesep, '.path_to_SCA.mat');
+    load(file_mat)
+    my_dir = strcat(path_to_local_folder,'src', filesep, 'MATLAB', filesep);
+    cd(my_dir)
+end
 files = dir;
 files(1:2) = []; % removing . ../
 dirFlags = [files.isdir];
