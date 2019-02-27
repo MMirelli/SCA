bad192861254c23135dc306b12c3933b70e57d8b
MMirelli <massimilianomirelli.mm@gmail.com>
2019-02-28 01:55:26 +0300

    Handling README.md in multitraces_converted

--- src/py/DPAv2/multi_traces_converter-py2.py
+++ src/py/DPAv2/multi_traces_converter-py2.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import glob
 
 """ This script "trims" all raw traces in /data/raw/DPAv2/public_db
 
@@ -25,6 +26,8 @@ data_path = home_path + "data" + sepr
 raw_traces_path = (data_path + "raw" + sepr + "DPAv2" +
                    sepr + "public_db"  + sepr)
 
+raw_csv = raw_traces_path + "*.csv"
+
 processed_traces_path = (data_path + "processed"  + sepr + "DPAv2" +
                          sepr + "public_db" + sepr)
 
@@ -32,13 +35,13 @@ i = 0
 
 print("Processing traces \n")
 
-for filename in os.listdir(raw_traces_path):
+for filename in glob.glob(raw_csv):
 
       i += 1
       t = open(processed_traces_path + "tr" + str(i) + ".csv", "wb")
       writer = csv.writer(t)
 
-      for row in csv.reader(open(raw_traces_path + filename, 'r')):
+      for row in csv.reader(open(filename, 'r')):
             if row[0][0]!='#':
                   writer.writerow(row)
       
