064d43b8593e938a4ef8b3134cc0376f1dc680e7
MMirelli <massimilianomirelli.mm@gmail.com>
2019-03-04 04:27:21 +0300

    Adding new py function to process trs without modifying names Adding stat procedure for ML bits predictions

--- src/py/DPAv2/multi_traces_converter-py2_ml.py
+++ src/py/DPAv2/multi_traces_converter-py2_ml.py
@@ -1,6 +1,7 @@
+import math
 import os
 import csv
-import glob
+import sys
 
 """ This script "trims" all raw traces in /data/raw/DPAv2/public_db
 
@@ -23,27 +24,31 @@ home_path = up_dir * 3
 # relative path to traces
 data_path = home_path + "data" + sepr
 
-raw_traces_path = (data_path + "raw" + sepr + "DPAv2" +
+raw_traces_path = (data_path + sepr + "raw" + sepr + "DPAv2" + #check two sepr why?????
                    sepr + "public_db"  + sepr)
 
 raw_csv = raw_traces_path + "*.csv"
 
-processed_traces_path = (data_path + "processed"  + sepr)
-
-i = 0
+processed_traces_path = (data_path + "ml" + sepr)
 
 print("Processing traces \n")
 
-for filename in glob.glob(raw_csv):
-
-      i += 1
-      t = open(processed_traces_path + "tr" + str(i) + ".csv", "wb")
+c = 0
+dir_l = os.listdir(raw_traces_path)
+for filename in dir_l:
+      c += 1
+      t = open(processed_traces_path + filename, "wb")
       writer = csv.writer(t)
 
-      for row in csv.reader(open(filename, 'r')):
+      for row in csv.reader(open(raw_traces_path + filename, 'r')):
             if row[0][0]!='#':
                   writer.writerow(row)
-      
+
       t.close()
 
+      
+      perc = c/(len(dir_l)/100)
+      s = '['+ perc*'#' + ' '*(100-perc) + '] ' + str(perc)+('% ') + "Processed: " + str(c)
+      sys.stdout.write('\r'+s)
+      
 print("Exiting, find traces in " + processed_traces_path) 
