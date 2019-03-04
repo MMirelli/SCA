45d03f921cfe79ec2df0bb444c93f840cb112235
MMirelli <massimilianomirelli.mm@gmail.com>
2019-03-04 19:42:12 +0300

    Adding procedure to print random traces and cwt of one of them

--- src/MATLAB/DPAv2/pattern_recognition/print_rand_trace_and_cwt.m
+++ src/MATLAB/DPAv2/pattern_recognition/print_rand_trace_and_cwt.m
@@ -5,14 +5,17 @@ function [] = print_rand_trace_and_cwt(traces_path, n_traces, groupsize)
 % It prints a csv-trace picked randomly among those stored in traces_path.
 % 
 % 
-    
-    r = randi(length(dir(traces_path)));
-    trace_name = strcat('tr', r);
+    Imax = length(dir(strcat(traces_path, '*.csv')));
+    r = randi(Imax);
+    trace_name = strcat('tr', num2str(r));
     trace = csvread(strcat(traces_path, trace_name, '.csv'));
+    
+    
     
     tmp_fold_name = 'fig-tmp';
     if ~exist(tmp_fold_name, 'dir')
        mkdir(tmp_fold_name);
+       fprintf('%s created...\n', tmp_fold_name);
     end
        
     fig_tr = figure(1);
@@ -24,47 +27,59 @@ function [] = print_rand_trace_and_cwt(traces_path, n_traces, groupsize)
     cwt(trace);
     cwt_name = strcat('cwt-', trace_name);
     cwt_full_path = strcat(tmp_fold_name, filesep, cwt_name);
-    savefig(fig_cwt, cwt_full_path);
-        
+    savefig(fig_cwt, cwt_full_path);    
+    
+    close(fig_tr, fig_cwt);
+    
     sub1=hgload(strcat(trace_full_path,'.fig'));
     sub2=hgload(strcat(cwt_full_path,'.fig'));
     
-    % Prepare subplots
-    ret_fig = figure(3);
     figs_path = strrep(traces_path, 'processed', 'figs');
     
-    if ~exist('n_traces','var')
-        h(1)=subplot(1,2,1);
-        h(2)=subplot(1,2,2);
-    else
-        load(strcat(figs_path, 'plot_', num2str(n_traces), ...
-            '_rand_traces-group_size',num2str(groupsize),'.mat'));    
-        % plot_rand_traces fig         
-        sub3 = fig;
-        
-        h(1)=subplot(1,3,1);
-        h(2)=subplot(1,3,2);
-        h(3)=subplot(1,3,3);
-        
+    loading_filename = strcat(figs_path, 'plot_', num2str(n_traces), ...
+        '_rand_traces'); 
+    
+    if nargin == 3
+       loading_filename = strcat(loading_filename, ...
+           '-group_size',num2str(groupsize));
     end
-    % Paste figures on the subplots
-    copyobj(allchild(get(sub1,'CurrentAxes')),h(1));
-    copyobj(allchild(get(sub2,'CurrentAxes')),h(2));
     
-    % Add legends
-    l(1)=legend(h(1),'LegendForFirstFigure')
-    l(2)=legend(h(2),'LegendForSecondFigure')
+    loading_filename = strcat(loading_filename, '.mat');
     
-    ret_fig_name = strcat('rand_trace', num2str(r), '_cwt');
-    
-    if exist('n_traces','var')
-        copyobj(allchild(get(sub3,'CurrentAxes')),h(3));
-        l(3)=legend(h(3),'LegendForSecondFigure')
-    
-        ret_fig_name = strcat(ret_fig_name, '-', num2str(n_traces), '-', num2str(groupsize));
-    end
+    load(loading_filename, 'fig'); 
+    sub3 = fig;
+        
+    % Prepare subplots
+    ret_fig = figure(4);
+             
    
-    save(strcat(figs_path, ret_fig_name,'.mat'), 'fig', '-v7.3');
-    delete(fig_tr, fig_cwt, ret_fig);
+    h(1)=subplot(3,1,1);
+    h(2)=subplot(3,1,2);
+    h(3)=subplot(3,1,3);
+    
+
+    % Paste figures on the subplots
+    copyobj(allchild(get(sub2,'CurrentAxes')),h(1));
+    fprintf('Subplotting %s\n', trace_name);
+    copyobj(allchild(get(sub3,'CurrentAxes')),h(2));
+    fprintf('Subplotting %s\n', cwt_name);
+    copyobj(allchild(get(sub1,'CurrentAxes')),h(3));
+%     fprintf('Subplotting %i random traces in %i group size\n', n_traces, groupsize);
+    
+    ret_fig_name = strcat('rand_trace', num2str(r), '_cwt-', ...
+        num2str(n_traces));
+    
+    if nargin == 3
+        ret_fig_name = strcat(ret_fig_name, '-', num2str(groupsize));
+    end
+    
+    ret_file = strcat(figs_path, ret_fig_name,'.mat');
+    save(ret_file, 'ret_fig', '-v7.3');
+    close(sub1, sub2, sub3);
+    
+    rmdir(tmp_fold_name, 's');
+    fprintf('%s removed...\n', tmp_fold_name);
+    fprintf('File %s saved.\nOpen it running:\nload %s\n', ret_fig_name, ret_file);
+    
     clear
 end
